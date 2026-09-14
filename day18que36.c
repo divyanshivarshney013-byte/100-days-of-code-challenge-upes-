e7b30d3f0897be474aed7e960b2f2b0db3f86c9d
Divyanshi Varshney <divyanshivarshney013-btye>
2026-09-14 10:51:30 +0530

    day 18 solution

---
+++ day18que36.c
@@ -0,0 +1,30 @@
+//Q36: Write a program to find the HCF (GCD) of two numbers.
+
+/*
+Sample Test Cases:
+Input 1:
+12 18
+Output 1:
+6
+
+Input 2:
+7 9
+Output 2:
+1
+
+*/
+
+#include <stdio.h>
+int main() {
+    int a, b, remainder;
+    printf("enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    while (b != 0) {
+        remainder = a % b;
+        a = b;
+        b = remainder;
+    }
+
+    printf("%d", a);
+}
